53f079cc9f4853138c39655af88f6e2c172f92b4
ryanha <ryanha@handong.ac.kr>
2023-04-29 20:21:10 +0900

    union-find 알고리즘 예시코드 tab space 불일치 문제 수정

--- algorithm-theory/union_find/Main.java
+++ algorithm-theory/union_find/Main.java
@@ -2,26 +2,25 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-
+        
         int[] nodes = new int[11];
-		for(int i=1 ; i<11 ; i++) {
-			nodes[i] = i;
-		}
-
-		union(nodes, 1, 2);
-		union(nodes, 2, 3);
-		union(nodes, 3, 4);
-		union(nodes, 5, 6);
-		union(nodes, 6, 7);
-		union(nodes, 7, 8);
-		union(nodes, 8, 9);
-		union(nodes, 9, 10);
-
-		System.out.println("1 and 4 is connected? = " + find(nodes, 1, 4));
-		System.out.println("3 and 9 is connected? = " + find(nodes, 3, 9));
-		union(nodes, 4, 5);
-		System.out.println("3 and 9 is connected? = " + find(nodes, 3, 9));
-
+        for(int i=1 ; i<11 ; i++) {
+            nodes[i] = i;
+        }
+        
+        union(nodes, 1, 2);
+        union(nodes, 2, 3);
+        union(nodes, 3, 4);
+        union(nodes, 5, 6);
+        union(nodes, 6, 7);
+        union(nodes, 7, 8);
+        union(nodes, 8, 9);
+        union(nodes, 9, 10);
+        
+        System.out.println("1 and 4 is connected? = " + find(nodes, 1, 4));
+        System.out.println("3 and 9 is connected? = " + find(nodes, 3, 9));
+        union(nodes, 4, 5);
+        System.out.println("3 and 9 is connected? = " + find(nodes, 3, 9));
     }
 
     private static int getParent(int[] nodes, int node) {
